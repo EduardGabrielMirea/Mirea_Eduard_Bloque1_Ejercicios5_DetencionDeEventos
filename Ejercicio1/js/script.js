2b6f5fb013f2af2789babc13df8facae556201b9
Eduard Gabriel Mirea <eduardmirea00@gmail.com>
2024-10-14 12:57:58 +0200

    commit

--- Ejercicio1/js/script.js
+++ Ejercicio1/js/script.js
@@ -1,4 +1,20 @@
 document.addEventListener("DOMContentLoaded",function () {
-    
-    
+
+    const form = document.getElementById("form");
+
+    form.addEventListener("submit",function (e) {
+        var nombre = document.getElementById("name").value;
+        var email = document.getElementById("email").value;
+        var comment = document.getElementById("comment").value;
+        var status = document.getElementById("status");
+
+        status.textContent = "";
+
+        if (nombre === "" || email === "" || comment === "") {
+            e.preventDefault();
+            status.textContent = "Error: Todos los campos son obligatorios.";
+        } else {
+            status.textContent = "Fromulario enviado con éxito.":
+        }
+    });
 });
